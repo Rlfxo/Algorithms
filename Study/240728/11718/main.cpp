db542339c9e0e1e7d1fe88664dc2c499e43aa4e0
yskl7137 <yskl7137@gmail.com>
2024-07-28 14:20:24 +0900

    11718 solved

---
+++ Study/240728/11718/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(void){
+    fstream fin("input.txt", ios::in);
+
+    string line;
+
+	while(true) {
+		getline(fin, line);
+		if(line == "") break;
+		cout << line << '\n';
+	}
+
+
+    return 0;
+}
+
+
