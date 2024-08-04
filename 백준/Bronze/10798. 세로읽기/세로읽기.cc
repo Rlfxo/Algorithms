767b9dc49d2c884508fc65b22cd8d8491b782f54
Gilbert <119564422+Rlfxo@users.noreply.github.com>
2024-08-04 17:43:40 +0900

    [Bronze I] Title: 세로읽기, Time: 4 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#define LINE_NUM (5)
+#define WORD_MAX (15)
+
+using namespace std;
+
+vector<vector<char>> vv;
+vector<char> v;
+
+int main(void){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    cin.exceptions(ios::badbit | ios::failbit);
+
+    int buffer, i, j;
+    string line;
+
+    for(i=0; i<LINE_NUM; i++){
+        getline(cin, line);
+
+        for(char c : line){
+            v.push_back(c);
+        }
+        vv.push_back(v);
+        v.clear();
+    }
+
+    for(i=0; i<WORD_MAX; i++){
+        for(j=0; j<LINE_NUM; j++){
+            try {
+                cout << vv[j].at(i);
+            }
+            catch (out_of_range& e){
+                continue;
+            }
+        }
+    }
+    return 0;
+}
