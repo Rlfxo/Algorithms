1382e5c0a2e449c10bb829e999c6f0339b0de952
Rlfxo <yskl7137@gmail.com>
2024-08-17 15:27:42 +0900

    9506 solved

---
+++ Study/240817/9506/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    cin.exceptions(ios::failbit | ios::badbit);
+
+    fstream fin("input.txt", ios::in);
+
+    while (true)
+    {
+        bool result = false;
+        int n, i, sum = 0;
+        fin >> n;
+        if (n == -1) break;
+
+        vector<int> v;
+        vector<int>::iterator itr = v.begin();
+        for(i = 1; i < n; i++){
+            if(n % i == 0) v.push_back(i);
+        }
+
+        for(itr = v.begin(); itr != v.end(); itr++){
+            sum += *itr;
+        }
+
+        if(sum == n){
+            cout << n << " = ";
+            for(itr = v.begin(); itr != v.end(); itr++){
+                if(itr == v.end() - 1) cout << *itr << endl;
+                else cout << *itr << " + ";
+            }
+        } else {
+            cout << n << " is NOT perfect." << endl;
+        }
+    }
+    return 0;
+}
