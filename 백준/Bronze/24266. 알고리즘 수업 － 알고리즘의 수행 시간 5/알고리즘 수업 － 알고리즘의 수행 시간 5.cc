2b857d4e66f392ab95fdcd88a50f8f0ddef953f9
Gilbert <119564422+Rlfxo@users.noreply.github.com>
2024-08-19 15:39:49 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdint.h>
+
+using namespace std;
+
+int main(void){
+    uint64_t n, i, j, k, code = 0;
+    cin >> n;
+    cout << (n * n * n) << endl;
+    cout << 3 << endl;
+    return 0;
+}
