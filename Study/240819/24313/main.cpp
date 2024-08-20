fee7b81104384828d5ea7d43ce33e5b991edc83a
Rlfxo <yskl7137@gmail.com>
2024-08-20 14:06:08 +0900

    24313 solved

--- Study/240819/24313/main.cpp
+++ Study/240819/24313/main.cpp
@@ -8,7 +8,15 @@ int main(void)
     uint64_t a0, a1, c, n, result = 0;
     cin >> a0 >> a1 >> c >> n;
 
+    if(a1 * n + a0 <= c * n && a1 <= c)
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        cout << 0 << endl;
+    }
     
 
     return 0;
-}
+} 
